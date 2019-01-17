f9acc3daa057e3d524b3f5b08aab0bb4418da39a
amperez09 <amperez@datiobd.com>
2019-01-17 12:35:31 -0600

    Final de proyecto

--- formacion/src/main/scala/com/mx/datio/App.scala
+++ formacion/src/main/scala/com/mx/datio/App.scala
@@ -1,15 +1,42 @@
 package com.mx.datio
+import scala.io.Source._
+import org.apache.log4j.Logger
 
-/**
- * @author ${user.name}
- */
 object App {
-  
+  val logger = Logger.getLogger(App.getClass)
+
   def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
-  
-  def main(args : Array[String]) {
-    println( "Hello World!" )
-    println("concat arguments = " + foo(args))
+
+  def words(args : Array[String],word:String):(Int,Int)={
+    logger.info("Inicia procesamiento")
+    var path = foo(args)                                                                                                //Obtiene path del archivo
+    var nlineas = 0
+    var words = 0
+    var arch = fromFile(path)
+    for (line <- arch.getLines) {                                                                                       //Obtiene lineas del archivo
+      if(line.mkString.contains(word)){                                                                                 //Encontrar Coincidencia
+        words+=1
+        println(line.mkString)
+      }
+      nlineas += 1
+    }
+    logger.info("Termina procesamiento")
+    return (words,nlineas)
   }
 
+
+  def main(args : Array[String])  {
+    val startTimeMillis = System.currentTimeMillis()                                                                    //Inicio de tiempo
+
+    var buscar="Multi-Family"                                                                                           //Coincidencia a encontrar
+    var (lineas,palabra)=words(args,buscar)                                                                             //llamada a funcion
+    logger.info("Imprimiendo ....")
+    println("Lineas del archivo ", lineas)
+    println("Coincidencias con Multi-Family", palabra)
+
+    val endTimeMillis = System.currentTimeMillis()
+    print("Tiempo: ms",endTimeMillis-startTimeMillis)                                                                                   //Fin de tiempo
+  }
 }
+
+
